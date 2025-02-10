cd7ff0ae72f6aa3fda38411c5e6a29367fce5e52
liyang <1987857842@qq.com>
2025-02-10 18:46:16 +0800

    feat: 优化next-i18next配置，保持语言设置和Suspense选项一致

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     i18n: {
-        locales: ['en', 'zh'], // 支持的语言列表
-        defaultLocale: 'en', // 默认语言
-        localeDetection: false, // 自动根据浏览器语言选择
+        locales: ['en', 'zh'], 
+        defaultLocale: 'en',
+        localeDetection: false,
     },
-    react: { useSuspense: false }, // 防止 Suspense 错误
+    react: { useSuspense: false },
 }
