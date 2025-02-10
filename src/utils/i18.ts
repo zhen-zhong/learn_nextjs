6c8021a54fe5c83a4a1d06b4460702edd8afc195
liyang <1987857842@qq.com>
2025-02-10 16:49:09 +0800

    feat: 更新国际化配置，添加中文翻译并禁用 Suspense

--- src/utils/i18.ts
+++ src/utils/i18.ts
@@ -1,23 +1,22 @@
-// src/i18n.ts
-
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import enTranslation from 'src/public/locales/en/common.json'
-import frTranslation from 'src/public/locales/zh/common.json'
 
-// 初始化 i18next
-i18n
-    .use(initReactI18next) // 使用 react-i18next
-    .init({
-        resources: {
-            en: { translation: enTranslation },
-            fr: { translation: frTranslation },
-        },
-        lng: 'en', // 默认语言
-        fallbackLng: 'en', // 如果语言加载失败则回退到英语
-        interpolation: {
-            escapeValue: false, // React 已经处理了 XSS 问题
-        },
-    })
+import en from 'src/public/locales/en/common.json'
+import zh from 'src/public/locales/zh/common.json'
+
+i18n.use(initReactI18next).init({
+    resources: {
+        en: { translation: en },
+        zh: { translation: zh },
+    },
+    lng: 'en', // 默认语言
+    fallbackLng: 'en', // 如果语言加载失败则回退到英语
+    interpolation: {
+        escapeValue: false,
+    },
+    react: {
+        useSuspense: false
+    }
+})
 
 export default i18n
