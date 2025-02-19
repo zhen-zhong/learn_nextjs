0e4278111d983416903b1e025c95953567ce7755
liyang <1987857842@qq.com>
2025-02-19 17:41:17 +0800

    feat: 更新国际化配置，添加useLocale钩子并将默认语言设置为中文

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-import { useTranslations } from 'next-intl';
+import { useTranslations, useLocale } from 'next-intl';
 const Home = () => {
   const t = useTranslations();
+  const locale = useLocale();
+  console.log(locale,'useLocale');
+  
   return (
     <>Home
       {t('test')}
